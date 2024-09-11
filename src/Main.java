d50a19f57f0ec0bd2f7c71772727b079c8c6e094
Username98673 <szymon.kitel@gmail.com>
2024-09-11 14:59:11 +0200

    wprowadzanie 6 liczb

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -9,14 +13,34 @@ public class Main {
          */
         int tablica_liczb_losowych[] = new int[6];
         for (int i = 0; i < tablica_liczb_losowych.length; i++) {
-            tablica_liczb_losowych[i]=(int)(Math.random()*100+1);//od 1 do 100
+            tablica_liczb_losowych[i] = (int) (Math.random() * 100 + 1);//od 1 do 100
         }
         //wypisywanie talicy na ekranie
         System.out.println("Wylosowana tablica; ");
-        for (int element:tablica_liczb_losowych) {
+        for (int element : tablica_liczb_losowych) {
             //(typ nazwa:nazwakolekcjii)
-            System.out.print(element+", ");
+            System.out.print(element + ", ");
+        }
+
+    /*kolekcje moga miec zmieniany rozmiar(dodawanie i usuwanie elementow)
+    kolekcje jako elementy tylko typy zlozone (z duzej litery)
+    list List - interfejs, ArrayList LinkedList
+    zbiory Set - interfejs, HashSet
+    mapy
+     */
+
+        //przygotu liste z liczbami pisanymi z klawiatury
+        ArrayList<Integer> lista_liczb_z_klawiatury = new ArrayList<>();
+        Scanner klawiatura = new Scanner(System.in);
+        System.out.println("podaj 6 liczb");
+        for (int i = 0; i < 6; i++) {
+            int liczba = klawiatura.nextInt();
+            lista_liczb_z_klawiatury.add(liczba);
+        }
+        //wpisz liczbe
+        System.out.println("Wprowadzona tablica");
+        for (int i = 0; i < lista_liczb_z_klawiatury.size(); i++) {
+            System.out.println(lista_liczb_z_klawiatury.get(i));
         }
     }
-
 }
